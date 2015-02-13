693520b57da603f05bfda199c703ce546f6dbe31
nielshehe <nieha12@student.sdu.dk>
2015-02-13 11:48:12 +0100

    Added NumberPlate

--- Cardealer/Domain/Vehicle/Vehicle.cs
+++ Cardealer/Domain/Vehicle/Vehicle.cs
@@ -10,6 +10,7 @@ namespace Domain.Vehicle
         public double Price { get; set; }
         public string Model { get; set; }
         public string Color { get; set; }
+        public string NumberPlate { get; set; }
 
         enum CarState { Commision, Sold, Leased };
     }

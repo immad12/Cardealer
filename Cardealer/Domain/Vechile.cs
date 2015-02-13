8366e5de2e4a67ff33ade9ea36e5681acd846442
immad12 <mette.kilde@gmail.com>
2015-02-13 10:08:15 +0100

    Created Vechile class with Car and Truck

---
+++ Cardealer/Domain/Vechile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Vechile
+    {
+        double price { get; set; }
+        String model { get; set; }
+        String Color{ get; set; }
+
+        enum CarState { Commision, Sold, Leased };
+
+        
+    }
+
+    class Car : Vechile
+    {
+ 
+    }
+
+    class Truck : Vechile
+    {
+
+    }
+}

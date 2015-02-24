c051d6da467d5d814ded0611f88c4bbf1a53566b
immad12 <mette.kilde@gmail.com>
2015-02-24 22:50:46 +0100

    Created contract, sale and leasing.cs
    To buy and lease vehicles

---
+++ Cardealer/Domain/Contracts/Leasing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain.Vehicle;
+
+
+namespace Domain.Contracts
+{
+    public class Leasing
+    {
+        //car
+        //Customer
+        //rent per month (rentprice)
+        //rent period
+        //startdate
+    }
+}
---
+++ Cardealer/Domain/Contracts/Sale.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Contracts
+{
+    class Sale
+    {
+        //Sale: Information of car and customer and date
+    }
+}

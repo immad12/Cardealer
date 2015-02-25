996e8d8dc55304de8edf09847d988db068883960
immad12 <mette.kilde@gmail.com>
2015-02-25 21:11:10 +0100

    Implemented Contract.cs and Leasing.cs

--- Cardealer/Domain/Contracts/Contract.cs
+++ Cardealer/Domain/Contracts/Contract.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Domain.Vehicle;
 
 namespace Domain.Contracts
 {
-    class Contract
+    public class Contract
     {
+        public Vehicles Vehicle { get; set; }
+        public Customer Customer { get; set; }
+
+        public Contract(Vehicles vehicle, Customer customer)
+        {
+            this.Vehicle = vehicle;
+            this.Customer = customer;
+        }
     }
 }
